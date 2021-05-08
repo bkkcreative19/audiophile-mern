4fc2e135b7088e8e9ba64b477e04efd1c9725236
Kyle King <bkkcreative16@gmail.com>
2021-05-08 14:00:13 -0500

    added home-categories

---
+++ client/src/components/HomeComponents/HomeCategories/HomeCategories.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Arrow from "../../../assets/assets/shared/desktop/icon-arrow-right.svg";
+import Headphones from "../../../assets/assets/shared/desktop/image-headphones.png";
+import EarPhones from "../../../assets/assets/shared/desktop/image-earphones.png";
+import Speakers from "../../../assets/assets/shared/desktop/image-speakers.png";
+
+const HomeCategories = () => {
+  return (
+    <div className="home-categories container">
+      <div className="cards">
+        <div className="card">
+          <img src={Headphones} alt="" />
+          <div className="content">
+            <h3>Headphones</h3>
+            <Link>
+              <span>Shop</span>
+              <img src={Arrow} alt="arrow" />
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <img src={Speakers} alt="" />
+          <div className="content">
+            <h3>Speakers</h3>
+            <Link>
+              <span>Shop</span>
+              <img src={Arrow} alt="arrow" />
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <img src={EarPhones} alt="" />
+          <div className="content">
+            <h3>EarPhones</h3>
+            <Link>
+              <span>Shop</span>
+              <img src={Arrow} alt="arrow" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCategories;
--- client/src/components/HomeComponents/index.js
+++ client/src/components/HomeComponents/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import HomeCategories from "./HomeCategories/HomeCategories";
 
 const HomeComponents = () => {
   return (
     <div>
       <Hero />
+      <HomeCategories />
     </div>
   );
 };
