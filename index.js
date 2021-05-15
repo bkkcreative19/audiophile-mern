36f4e4b340744a654d62ba874e6e6ac956526348
Kyle King <bkkcreative16@gmail.com>
2021-05-15 12:41:01 -0500

    deploy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import path from "path";
 
 // route imports
 import productRouter from "./routes/product.js";
@@ -14,11 +15,15 @@ const app = express();
 connectDB();
 
 // middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
 // routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.use("/api", productRouter);
 
 const port = process.env.PORT || 5001;
