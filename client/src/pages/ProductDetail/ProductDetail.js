6cf90da81552f412371bbb36724a5244e0f124ba
Kyle King <bkkcreative16@gmail.com>
2021-05-15 12:19:28 -0500

    cleaned

--- client/src/pages/ProductDetail/ProductDetail.js
+++ client/src/pages/ProductDetail/ProductDetail.js
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import ProductDetailComponents from "../../components/ProductDetailComponents";
-import data from "../../data.json";
 
 const ProductDetail = () => {
-  const params = useParams();
-  const [product, setProduct] = useState({});
-
-  useEffect(() => {
-    const product = data.find((item) => item.name === params.name);
-
-    setProduct(product);
-  }, [params.name]);
-
   return (
     <>
-      <ProductDetailComponents product={product} />
+      <ProductDetailComponents />
     </>
   );
 };
